62c3e64f3c06057469087ed43a46cac652eee0ae
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-05 11:13:53 -0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
